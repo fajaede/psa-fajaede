cbbb8bfee786926d8921091a3fcd64528620a3ee
fajaede <fajaede@live.nl>
2025-11-28 16:20:04 +0100

    Fix PrismaClient import for Vercel

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,10 +1,15 @@
 // lib/prisma.ts
-import { PrismaClient } from "@prisma/client";
+import * as PrismaPkg from "@prisma/client";
+
+const { PrismaClient } = PrismaPkg;
 
 declare global {
-  var prisma: PrismaClient | undefined;
+  // eslint-disable-next-line no-var
+  var prisma: InstanceType<typeof PrismaClient> | undefined;
 }
 
 export const prisma = global.prisma ?? new PrismaClient();
 
-if (process.env.NODE_ENV !== "production") global.prisma = prisma;
+if (process.env.NODE_ENV !== "production") {
+  global.prisma = prisma;
+}
